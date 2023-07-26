92672086881e6ee7c3f5f94557a34e145f6f9f3e
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-26 13:41:14 +0200

    InterfaceSegregation

---
+++ DP/SOLID/IV_I.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.SOLID.I
+{
+    /*interface IFormatter
+    {
+        void ToExcel();
+        void ToPdf();
+    }*/
+    interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+    interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+    /*interface IFormatter : IPdfFormatter, IExcelFormatter
+    {
+    }*/
+
+    class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IPdfFormatter, IConvertible
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+}
