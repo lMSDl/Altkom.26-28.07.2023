5095115f2c4e865a8912f182602d512e485b0710
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-27 15:27:53 +0200

    PrivateClassData

---
+++ DP/DesignPatterns/Structural/PrivateClassData/ClassData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.DesignPatterns.Structural.PrivateClassData
+{
+    internal class ClassData
+    {
+        //private /*readonly*/ int _intValue;
+        //private string _stringValue;
+
+        private PrivateClassData _privateClassData;
+
+        public ClassData(int intValue, string stringValue)
+        {
+            /*_intValue = intValue;
+            _stringValue = stringValue;*/
+            _privateClassData = new PrivateClassData(intValue, stringValue);
+        }
+
+        public void DoSth()
+        {
+
+            _privateClassData.GetStringValue();
+            _privateClassData.GetIntValue();
+
+            //_stringValue = "";
+        }
+    }
+}
---
+++ DP/DesignPatterns/Structural/PrivateClassData/PrivateClassData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.DesignPatterns.Structural.PrivateClassData
+{
+    internal class PrivateClassData
+    {
+        private int _intValue;
+        private string _stringValue;
+        public PrivateClassData(int intValue, string stringValue)
+        {
+            _intValue = intValue;
+            _stringValue = stringValue;
+        }
+        public int GetIntValue()
+        {
+            return _intValue;
+        }
+
+        public string GetStringValue()
+        {
+            return _stringValue;
+        }
+    }
+}
