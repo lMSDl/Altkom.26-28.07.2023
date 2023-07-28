dc2a530a3d0e55a345f8f0414d9f92bdc509a991
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-28 11:25:21 +0200

    Mediator

---
+++ DP/DesignPatterns/Behavioral/Mediator/ChatMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.DesignPatterns.Behavioral.Mediator
+{
+    internal abstract class ChatMember
+    {
+        private IMediator mediator;
+        public string Nick { get; }
+        public IMediator Mediator
+        {
+            get => mediator;
+            set
+            {
+                mediator?.Quit(this);
+                mediator = value;
+                mediator?.Join(this);
+            }
+        }
+
+        protected ChatMember(string nick)
+        {
+            Nick = nick;
+        }
+
+        public abstract void Receive(string from, string message, bool isPrivate);
+
+
+    }
+}
